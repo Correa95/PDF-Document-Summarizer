31fe4a6ecea9834aca60fdec9dfe9bee39a457ab
Correa95 <mathewcorrea19@gmail.com>
2025-09-03 09:31:36 -0400

    styling the application

--- Frontend/document-summarizer/src/App.jsx
+++ Frontend/document-summarizer/src/App.jsx
@@ -3,8 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="appContainer">
+      <h1>Well Come to your Document Summarizer</h1>
       <form>
-        <label>upload your Document</label>
+        <label>
+          upload your Document:
+          <input type="text" className="input" />
+        </label>
       </form>
     </div>
   );
