efe91ab96ba6e77a8d347a392eab6cae8616a55b
Correa95 <mathewcorrea19@gmail.com>
2025-09-03 09:30:13 -0400

    styling the application

--- Frontend/document-summarizer/src/App.jsx
+++ Frontend/document-summarizer/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="appContainer"></div>;
+  return (
+    <div className="appContainer">
+      <form>
+        <label>upload your Document</label>
+      </form>
+    </div>
+  );
 }
 
 export default App;
